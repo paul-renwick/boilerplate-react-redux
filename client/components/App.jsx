b177ebb3fc61a4279875fdff781195dcd161fac6
Paul <prenwick@gmail.com>
2019-05-20 22:07:13 +1200

    Playing around with Material UI components and layouts.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Component Imports
 import Test from './Test'
+import SignIn from './SignIn'
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Test} />
+        <Route exact path='/SignIn' component={SignIn}/>
       </Switch>
     </Router>
   )
--- client/components/Test.jsx
+++ client/components/Test.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import Typography from '@material-ui/core/Typography';
+
 class Test extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <h1>This is a test.</h1>
-        <Link to='/'><h2>This link goes nowhere.</h2></Link>
+        <Typography variant='h1'>This is a test.</Typography>
+        <Link to='/SignIn'><h2>This link goes nowhere.</h2></Link>
 
       </React.Fragment>
 
